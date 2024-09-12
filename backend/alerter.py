e6d4c69c125dcd6b8c06d8c1e576e47520ed34d4
tinfoilcipher <andy@tinfoilcipher.co.uk>
2024-09-12 16:19:14 +0100

    feat: add basic frontend

--- backend/alerter.py
+++ backend/alerter.py
@@ -1,19 +1,22 @@
 import smtplib
 from email.message import EmailMessage
 from datetime import datetime
+from logger import log_writer
 
 def email_alerter(body, config):
-    now = datetime.now()
-    alert_time = now.strftime("%d-%m-%Y--%H-%M-%S")
-    subject = (f"Monitoring Alert {alert_time}")
+    if config["enabled"] is True:
+        log_writer(f"Raising Email Alert")
+        now = datetime.now()
+        alert_time = now.strftime("%d-%m-%Y--%H-%M-%S")
+        subject = (f"Monitoring Alert {alert_time}")
 
-    message = EmailMessage()
-    message.set_content("THIS IS THE BODY OF MY EMAIL")
-    message['Subject'] = subject
-    message['From'] = config["sender"]
-    message['To'] = config["recipient"]
+        message = EmailMessage()
+        message.set_content(body)
+        message['Subject'] = subject
+        message['From'] = config["sender"]
+        message['To'] = config["recipient"]
 
-    # Currently only support local, open SMTP relay without auth. Yuck
-    client = smtplib.SMTP(config["server"], config["port"])
-    client.send_message(message)
-    client.quit()
+        # Currently only support local, open SMTP relay without auth. Yuck
+        client = smtplib.SMTP(config["server"], config["port"])
+        client.send_message(message)
+        client.quit()
--- backend/app.py
+++ backend/app.py
@@ -6,16 +6,16 @@ from time import sleep
 #--Variable Loading
 with open('vars.yaml', 'r') as varfile:
      vars = safe_load(varfile)
+     polling_interval = vars["polling_interval"]
      ip_config = vars["ip"]
      http_config = vars["http"]
-     polling_interval = vars["polling_interval"]
-     mailer_config = vars["mail"]
+     alerter_config = vars["alerting"]
 
 #--Application startup
 if __name__ == "__main__":
-     target = connection_tester(
+     connection_tester(
           polling_interval,
           ip_config,
           http_config,
-          mailer_config
+          alerter_config
           )
